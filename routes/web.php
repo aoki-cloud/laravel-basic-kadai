452e3ccd34cfad4482f65829d97e8007edd90394
aoki-cloud <takosukeaoki@icloud.com>
2024-09-23 10:19:35 +0900

    課題更新

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +15,26 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
 
 Route::get('/posts', [PostController::class, 'index']);
 
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
